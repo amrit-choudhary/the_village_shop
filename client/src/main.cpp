f5bc542e7e25241382a6a884998d5dddf961b436
AmritChoudhary <albela.amrit@gmail.com>
2025-03-10 11:13:24 +0530

    Implementation of game's frame timer and deltaTime. Current fps = 2.68149e+07

--- client/src/main.cpp
+++ client/src/main.cpp
@@ -1,7 +1,42 @@
 #include <iostream>
+#include <thread>
 
-int main(int argc, char** argv)
+#include "TimeManagement/TimeManager.h"
+
+namespace
+{
+    const uint32_t runTimeSeconds = 10;
+    double initPos = 0;
+    double speed = 100; // 100 m/s;
+}
+
+int main(int argc, char **argv)
 {
 
-    std::cout << "Hello World from client!" << '\n';
+    std::cout << "Game Start!" << '\n';
+
+    // Init global variables.
+    TimeManager timeManager;
+    timeManager.GameStart();
+
+    while (true)
+    {
+        timeManager.Update();
+
+        initPos += speed * timeManager.GetDeltaTime();
+
+        if (timeManager.GetTimeSinceStartup() > runTimeSeconds)
+        {
+            break;
+        }
+    }
+
+    timeManager.GameEnd();
+
+    std::cout << "Final Pos: " << initPos << '\n';
+
+    std::cout << "Game Run Time: " << timeManager.GetTimeSinceStartup() << '\n';
+    std::cout << "Average FPS: " << timeManager.GetFrameCount() / timeManager.GetTimeSinceStartup() << '\n';
+    std::cout << "Total Frames: " << timeManager.GetFrameCount() << '\n';
+    std::cout << "Game Over!" << '\n';
 }
